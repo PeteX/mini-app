f8301387ffc6e441e1b0b7712e7abf0a567e1b62
Pete Chown <pete@chown.org.uk>
2020-04-28 20:18:56 +0100

    Update lit-element to 2.3.1.  (The earlier file is still available.)

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import { terser } from 'rollup-plugin-terser';
 export default {
   input: 'node_modules/lit-element/lit-element.js',
   output: {
-    file: 'public/static/lit-element/2.2.1/lit-element-01.js',
+    file: 'public/static/lit-element/2.3.1/lit-element-01.js',
     format: 'esm',
     sourcemap: true
   },
